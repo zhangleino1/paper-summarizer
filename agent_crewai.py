fd8a2052a7c4511882d94b0411bd97d0aece16d5
zhanglei <zhangleilikejay@gmail.com>
2024-09-10 11:02:33 +0800

    优化

--- agent_crewai.py
+++ agent_crewai.py
@@ -19,41 +19,65 @@ import re
 from urllib.parse import urlparse, parse_qs, unquote
 from crewai import Agent, Task, Crew
 from langchain_ollama import ChatOllama
-
+Model = "qwen2:7b"
 # 设置 Ollama API 环境变量
 # os.environ["OLLAMA_API_KEY"] = "your_ollama_api_key"
-llm = ChatOllama(model="qwen2:7b", base_url="http://localhost:11434")
+llm = ChatOllama(model=Model, base_url="http://localhost:11434")
+
+# Load environment variables
+load_dotenv()
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# QQ email IMAP settings
+IMAP_SERVER = 'imap.qq.com'
+EMAIL_ACCOUNT = os.getenv('QQ_EMAIL')
+PASSWORD = os.getenv('QQ_PASSWORD')
+
+# Firecrawl API settings
+FIRECRAWL_API_KEY = os.getenv('FIRECRAWL_API_KEY')
+FIRECRAWL_API_URL = 'http://140.143.139.183:3002/v1'
+
+# Define the email criteria
+SENDER_EMAIL = 'scholaralerts-noreply@google.com'
+DAYS_RECENT = 2  # Set this to the number of recent days you want to filter emails by
 
 
+
+os.environ['CREWAI_DISABLE_TELEMETRY'] = 'true'
 # 定义内容清理 Agent
-clean_content_agent = Agent(
-    role="网页内容清理专家",
-    goal="清理并结构化网页内容，去除不必要的元素，提取有意义的文本。",
-    backstory="你是清理网页内容的专家，专注于将网页内容结构化为清晰、可读的Markdown格式。",
-    allow_delegation=False,
-    verbose=True,
-    llm=llm
-)
+def clean_content_agent():
+    return  Agent(
+                role="网页内容清理专家",
+                goal="清理并结构化网页内容，去除不必要的元素，提取有意义的文本。",
+                backstory="你是清理网页内容的专家，专注于将网页内容结构化为清晰、可读的Markdown格式。",
+                allow_delegation=False,
+                verbose=True,
+                llm=llm
+            )
 
 # 定义翻译 Agent
-translate_agent = Agent(
-    role="学术翻译专家",
-    goal="将清理后的学术内容翻译成中文，确保学术严谨性和清晰度。",
-    backstory="你是一名专业翻译，擅长学术文本的翻译，确保技术术语在中文翻译中的准确性。",
-    allow_delegation=False,
-    verbose=True,
-    llm=llm
-)
+def translate_agent():
+    return Agent(
+        role="学术翻译专家",
+        goal="将清理后的学术内容翻译成中文，确保学术严谨性和清晰度。",
+        backstory="你是一名专业翻译，擅长学术文本的翻译，确保技术术语在中文翻译中的准确性。",
+        allow_delegation=False,
+        verbose=True,
+        llm=llm
+    )
 
 # 定义总结 Agent
-summarize_agent = Agent(
-    role="研究总结专家",
-    goal="总结翻译后的内容，重点包括背景、研究问题、方法和创新点。",
-    backstory="你是一名经验丰富的学术研究人员，能够将复杂的论文进行梳理，总结，方便阅读。",
-    allow_delegation=False,
-    verbose=True,
-    llm=llm
-)
+def summarize_agent():
+    return Agent(
+        role="研究总结专家",
+        goal="总结翻译后的内容，重点包括背景、研究问题、方法和创新点。",
+        backstory="你是一名经验丰富的学术研究人员，能够将复杂的论文进行梳理，总结，方便阅读。",
+        allow_delegation=False,
+        verbose=True,
+        llm=llm,
+    )
 
 def create_clean_content_task(markdown_content):
     return Task(
@@ -76,26 +100,27 @@ def create_summarize_task():
         expected_output="总结后的论文内容，以Markdown格式呈现，保留原始结构和标题，去掉任何无关内容。"
     )
 
-# Load environment variables
-load_dotenv()
 
-# Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# QQ email IMAP settings
-IMAP_SERVER = 'imap.qq.com'
-EMAIL_ACCOUNT = os.getenv('QQ_EMAIL')
-PASSWORD = os.getenv('QQ_PASSWORD')
 
-# Firecrawl API settings
-FIRECRAWL_API_KEY = os.getenv('FIRECRAWL_API_KEY')
-FIRECRAWL_API_URL = 'http://140.143.139.183:3002/v1'
+def ollama_request(prompt):
+    try:
+        response = requests.post('http://localhost:11434/api/chat', json={
+            "model": Model,
+            "messages": [{"role": "user", "content": prompt}],
+            "stream": False
+        })
+        response.raise_for_status()
+        return response.json()['message']['content']
+    except requests.RequestException as e:
+        logging.error(f"Error in Ollama request: {e}")
+        return None
 
-# Define the email criteria
-SENDER_EMAIL = 'scholaralerts-noreply@google.com'
-DAYS_RECENT = 2  # Set this to the number of recent days you want to filter emails by
+def get_paper_type(content):
+    prompt = f"判断以下内容是关于大模型/AI Agent 相关的论文，还是室内定位/惯性导航相关的论文。返回文献类型：'大模型/AI Agent' 或 '室内定位/惯性导航'，如果不属于这两种类型，返回 '忽略'。\n\n{content}"
+    return ollama_request(prompt)
 
-Model = "qwen2:7b"
+
 
 @backoff.on_exception(backoff.expo, imaplib.IMAP4.error, max_tries=5)
 def get_emails():
@@ -129,6 +154,7 @@ def decode_content(part):
 def fetch_email_content(mail, email_id):
     try:
         logging.info(f"Fetching email ID: {email_id}")
+        print(f"Fetching email ID: {email_id}")
         result, data = mail.fetch(email_id, "(RFC822)")
         if result != 'OK':
             logging.error(f"Failed to fetch email ID: {email_id}, result: {result}")
@@ -147,6 +173,7 @@ def fetch_email_content(mail, email_id):
         now = datetime.now(email_date.tzinfo)
         if email_date < now - timedelta(days=DAYS_RECENT):
             logging.info(f"Email ID {email_id} is older than the specified range.")
+            print(f"Email ID {email_id} is older than the specified range.")
             return None
 
         # Extract content from the email
@@ -164,29 +191,6 @@ def fetch_email_content(mail, email_id):
         logging.error(f"Error getting email content for email ID: {email_id}: {e}")
         return None
 
-
-
-headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'cookie': 'HSID=A_DD98WEIxsZsZc1D; SSID=Acev8bMzZMZ-CHBqd; APISID=ol9iD2CnHWFkjR6A/AphA9fyLgdows48j1; SAPISID=bAFbrV_vxE88zUON/AAEQDZY3Ih52DjQC5; __Secure-1PAPISID=bAFbrV_vxE88zUON/AAEQDZY3Ih52DjQC5; __Secure-3PAPISID=bAFbrV_vxE88zUON/AAEQDZY3Ih52DjQC5; GSP=LM=1713536331:S=Ht2EeIZJY6yo2fZm; SEARCH_SAMESITE=CgQI15sB; SID=g.a000mgg5DJ-LGRng_4NuGlwwJt7hy3rrcY8qMQEuphLK51otZLugCQoRX_ZOGhvP7J3Rn96EYgACgYKAc4SARUSFQHGX2MihSJ1TjKlro8BtaQxUFPIFRoVAUF8yKpQEdi69tnxJWObbNCwGRwP0076; __Secure-1PSID=g.a000mgg5DJ-LGRng_4NuGlwwJt7hy3rrcY8qMQEuphLK51otZLugDJa2sr68sT5C5rIL9Y-7awACgYKAdoSARUSFQHGX2MiNg8zEzovcDHkdr4cgF3spBoVAUF8yKoYKdFZNXJBOE27-ktp_7pG0076; __Secure-3PSID=g.a000mgg5DJ-LGRng_4NuGlwwJt7hy3rrcY8qMQEuphLK51otZLugtlCOMMTsX_xLmhJoO9xZBQACgYKAQASARUSFQHGX2MiKvDoa4FyklqtfomMLEWNbRoVAUF8yKo2QJIh6OtPUA33Y1tEMo2J0076; OGPC=19026797-10:19026792-1:; AEC=AVYB7crBKOo4i964_5eZ8CCIJaZF8UWX2_LOibVbtHdFs1_KiLwhwYHHkVY; mp_851392464b60e8cc1948a193642f793b_mixpanel=%7B%22distinct_id%22%3A%20%22%24device%3A191a8b2019c54c-033061441b06e-26001051-1fa400-191a8b2019c54c%22%2C%22%24device_id%22%3A%20%22191a8b2019c54c-033061441b06e-26001051-1fa400-191a8b2019c54c%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22%24search_engine%22%3A%20%22google%22%7D; NID=517=AoAMxNP9bhSOqgEQcSGFD0iIr4iD7bakt1iS_O7k2vVCTQbQRIRoPv-xfmnti1Tbkol0XPIymFXqnGQfjaABZS4AO5NLHvc1jbfO1f1W9J8SGe-S09L8XeOb4gWwmc7bGzqTdciYNbr-s4qtE8u19kORMTZpF83CJ4K76QTRnO4r5gD1y9FAGBiuZU3BEAAsZ4vSD_65XsURB0menj3fT7f6SL1T3dpKGMs3CYcvHTGeNpmg28cZcBoy9lSLpL_DgUqANemzSH0M0122OcOxZQDbnyzvIbJFwjxfDZvHu_JmsAXhkaV3CvFWchEuoQwVeI4KD00I7BECid1jK0t3kfKJmoK-Krs5pwnjH5tlBQc1S9wch63iwRaHuWw; __Secure-1PSIDTS=sidts-CjIBUFGoh9Vky5sr-opqPumhtnS-qvPlJFg6tNH4gHNEwhLXNPFy6ZOBv1BvVcg2tA4TMhAA; __Secure-3PSIDTS=sidts-CjIBUFGoh9Vky5sr-opqPumhtnS-qvPlJFg6tNH4gHNEwhLXNPFy6ZOBv1BvVcg2tA4TMhAA; SIDCC=AKEyXzUDbTwWHUL1HgPSCPqT3ihD2tCB7i1sz_7v6U2AEAitv3NKQ9yPXIaABW_z_Qlp-oWnL8s; __Secure-1PSIDCC=AKEyXzXKYtFYJ65HpUdHecxScdqtX5_GESv0JgFUDc5TZxlQPbwk2eQpEAqFR4YCzRiJNKuSpBmE; __Secure-3PSIDCC=AKEyXzUlsC7gD9jgjrS4U7iYLyfwUxySJJIbZRMSsdodrW830O7SMg8uKnawUmh0ycg_lmnhaRk',
-        'priority': 'u=0, i',
-        'sec-ch-ua': '"Not=A?Brand";v="8", "Chromium";v="129", "Google Chrome";v="129"',
-        'sec-ch-ua-arch': '"x86"',
-        'sec-ch-ua-bitness': '"64"',
-        'sec-ch-ua-full-version-list': '"Not=A?Brand";v="8.0.0.0", "Chromium";v="129.0.6668.12", "Google Chrome";v="129.0.6668.12"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-model': '""',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-ch-ua-platform-version': '"19.0.0"',
-        'sec-ch-ua-wow64': '?0',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
-    }
 
 def get_final_url(input_url):
     try:
@@ -291,20 +295,44 @@ def process_paper(url):
     markdown_content = firecrawl_crawl(url)
     logging.info(f"Processing paper markdown_content: {markdown_content}")
     if markdown_content and markdown_content['markdown']:
+         # 判断类型
+        paper_type = get_paper_type(markdown_content['markdown'])  # 假设第一个任务返回文献类型
+        logging.info(f"Paper type: {paper_type}")
+        print(f"Paper type: {paper_type}")
+        if paper_type == "忽略":
+            logging.info(f"Ignoring paper from URL: {url}")
+            print(f"Ignoring paper from URL: {url}")
+            return None
+        # 添加类型判断
         crew = Crew(
-            agents=[clean_content_agent, translate_agent, summarize_agent],
+            agents=[ clean_content_agent(), translate_agent(), summarize_agent()],
             tasks=[
                 create_clean_content_task(markdown_content['markdown']),
                 create_translate_task(),
                 create_summarize_task()
-            ]
+            ],
+            telemetry=False,
+            verbose=True
         )
 
         result = crew.kickoff()
         
+       
+
+        # 根据类型设置文件名称
+        now = datetime.now()
+        now_str = now.strftime("%Y%m%d")
+        if paper_type == "大模型/AI Agent":
+            output_file = f"{now_str}_大模型_AIAgent.md"
+        elif paper_type == "室内定位/惯性导航":
+            output_file = f"{now_str}_室内定位_IMU.md"
+        else:
+            logging.warning(f"Unrecognized type for paper from URL: {url}")
+            return None
+
         # Format the final output
-        formatted_output = f"""{result} \n\n## 原文链接{url} \n\n"""
-        return formatted_output
+        formatted_output = f"""{result} \n\n## 原文链接\n{url} \n\n"""
+        return output_file, formatted_output
     return None
 
 def main():
@@ -314,20 +342,22 @@ def main():
         return
 
     all_paper_urls = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-        email_contents = list(executor.map(lambda email_id: fetch_email_content(mail, email_id), email_ids))
-        for content in email_contents:
-            if content:  # Filter out None values
-                all_paper_urls.extend(extract_urls(content))
+    for email_id in email_ids:
+        content = fetch_email_content(mail, email_id)
+        if content:
+            all_paper_urls.extend(extract_urls(content))
 
-        results = list(executor.map(process_paper, all_paper_urls))
-    # 取年月日 时分秒作为文件名
-    now = datetime.now()
-    now_str = now.strftime("%Y%m%d%H%M%S")
-    with open(now_str+'.md', 'w', encoding='utf-8') as f:
-        for result in results:
-            if result:
-                f.write(f"{result}\n\n")
+    for url in all_paper_urls:
+        result = process_paper(url)
+        if result:
+            output_file, formatted_output = result
+            with open(output_file, 'a', encoding='utf-8') as f:
+                f.write(f"{formatted_output}\n\n")
+            logging.info(f"Processed and wrote result for URL: {url}")
+        else:
+            logging.warning(f"Failed to process URL: {url}")
+
+    logging.info("All papers processed.")
             
 
 if __name__ == "__main__":
